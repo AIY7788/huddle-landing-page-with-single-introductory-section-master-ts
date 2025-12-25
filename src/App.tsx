550eeef84212fe105d3c3f6873e4324b43af6c55
AIY7788 <aiykhounnakhon@gmail.com>
2025-12-25 11:36:28 +0700

    feat: update App component layout and add Font Awesome icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <img src="/logo.svg" className="logo" alt="Logo" />
+
+        <main>
+          <img
+            src="/images/illustration-mockups.svg"
+            alt="Illustration Mockups"
+          />
+
+          <section>
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
+
+            <a href="#register" className="btn-register">
+              Register
+            </a>
+          </section>
+
+          <footer>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+            <a href="#" className="icon">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+          </footer>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
